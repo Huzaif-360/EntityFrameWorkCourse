9c136f7c7c09211c0c8f2601c7b097c6af5e3b53
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-13 14:50:01 +0500

    Completed EFC Lecture 6 WorkFlows

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -12,8 +12,21 @@ namespace EntityFrameWorkCourse
         {
             //Section 2
             //Getting Started
-            //Lecture 5
-            //What is Entity FrameWork
+            //Lecture 6
+            //WorkFlows:
+            /*
+             * Workflows:
+             *  1.Code First
+             *          We do code first and then we do wokr on database
+             *  
+             *  2.DataBase First:
+             *          We do database work first and then we do coding part
+             *  3.Model First:
+             *          We model the problem and then decide what to do, it includes UML designing but this is not liked much
+             *          
+             *    
+             *  1 and 2 are famous 
+            */
             Console.WriteLine();
         }
     }
