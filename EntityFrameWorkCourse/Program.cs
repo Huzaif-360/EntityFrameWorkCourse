3d134463883ec500d979f0897f7d55e24965d0b3
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-14 17:10:46 +0500

    Completed EFC Lecture 9 Database-First or Code-First?

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -12,26 +12,16 @@ namespace EntityFrameWorkCourse
     {
         static void Main(string[] args)
         {
-        //Section 2
-        //Getting Started
-        //Lecture 8
-        //Demo: Code - First Workflow
+            //Section 2
+            //Getting Started
+            //Lecture 9
+            //Completed Database-First or Code-First?
+
 
 
         }
 
     }
 
-    public class Post
-    {
-        public int Id { get; set; }
-        public DateTime DatePublished { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-    public  class BlogContext : DbContext
-    {
-        public DbSet<Post> Posts { get; set; }
-
-    }
+    
 }
