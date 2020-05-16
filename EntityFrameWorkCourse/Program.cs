ec90d1dc54085383a052aa85f351db580114fef5
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-17 02:46:01 +0500

    Completed EFC Section 3: Building a Model using Database-First Workflow

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -15,8 +15,8 @@ namespace EntityFrameWorkCourse
             /*
              * Section 3: Building a Model using Database-First Workflow
              * 
-             * Lecture 19
-             *Working with Enums
+             *
+             *Completed Section 3
              * 
 
             */
