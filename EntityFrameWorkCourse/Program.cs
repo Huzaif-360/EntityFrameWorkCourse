ead6ac7dbdc4347341e7eb73c7eafb9b54c16a0b
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-17 02:40:45 +0500

    Completed EFC Lecture 14 Uncovering the EDMX

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -15,10 +15,10 @@ namespace EntityFrameWorkCourse
             /*
              * Section 3: Building a Model using Database-First Workflow
              * 
-             * Lecture 13
-             * A Quick Tour of EDMX Designer
+             * Lecture 14
+             *Uncovering the EDMX
              * 
-             * Whenever we make changes in the file Entity FrameWork creates/ Generates domain classes
+
             */
 
 
