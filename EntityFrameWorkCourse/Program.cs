28fce6af82f14db98228fdb54f2eb190757c1bb2
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-17 02:42:54 +0500

    Completed EFC Lecture 16  Dealing with Database Changes

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -15,8 +15,8 @@ namespace EntityFrameWorkCourse
             /*
              * Section 3: Building a Model using Database-First Workflow
              * 
-             * Lecture 15
-             *Connection Strings
+             * Lecture 16
+             * Dealing with Database Changes
              * 
 
             */
