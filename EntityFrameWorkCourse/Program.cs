3b5b8ce5ca75b15d4a4b20a72ca6fae256f3d31e
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-18 20:52:11 +0500

    First Commit EFC Lecture 25 Migrations: Enabling Migrations

--- EntityFrameWorkCourse/Program.cs
+++ EntityFrameWorkCourse/Program.cs
@@ -4,61 +4,15 @@ using System.Data.Entity;
 
 
 
-//Lecture 24
-//Completed
-//Code First with an Existing
-//Database
-//Model
-//Migration
-// In migration we only run program and make it update the database in SQL Server 
+//Lecture 25
+/*
+ * Migrations: Enabling Migrations
+ * with the code first workflow first we change the model and then we use migrations to bring the database up to date.
+ */ 
 
 namespace CodeFirst
 {
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public CourseLevel Level { get; set; }
-        public float FullPrice { get; set; }
-        public Author Author { get; set; }
-        public IList<Tag> Tags { get; set; }
-    }
-
-    public class Author
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public IList<Course> Courses { get; set; }
-    }
-
-    public class Tag
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public IList<Course> Courses { get; set; }
-    }
-
-    public enum CourseLevel
-    {
-        Beginner = 1,
-        Intermediate = 2,
-        Advanced = 3
-    }
-
-    public class PlutoContext : DbContext
-    {
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-
-        //after making a string connection we have to tell Entity framework explicitly that we have added what name of string
-        public PlutoContext()
-            : base("name=DefaultConnection")
-        {
-            
-        }
-    }
+   
 
     class Program
     {
